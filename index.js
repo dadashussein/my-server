6f13d1b235b23d7106878bdcb9d9199e1912fbc9
Dadash Hussein <huseynzade.dadas@gmail.com>
2023-05-02 20:08:58 +0400

    okey

--- index.js
+++ index.js
@@ -2,23 +2,14 @@ import dotenv from "dotenv";
 import express from "express";
 import mongoose from "mongoose";
 import cors from "cors";
-import Product from "./models/Product.js";
+import productsRouter from "./routes/productsRouter.js";
 
 dotenv.config();
 const app = express();
 
 app.use(cors());
 app.use(express.json());
-
-app.get("/products", async (req, res) => {
-  try {
-    const products = await Product.find();
-    res.json(products);
-  } catch (error) {
-    console.log(error);
-    res.status(500).json({ message: "Serverde xeta var" });
-  }
-});
+app.use("/products", productsRouter);
 
 const PORT = process.env.PORT || 8000;
 
---
+++ routes/productsRouter.js
@@ -0,0 +1,16 @@
+import express from "express";
+import Product from "../models/Product.js";
+
+const router = express.Router();
+
+router.get("/", async (req, res) => {
+  try {
+    const products = await Product.find();
+    res.json(products);
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({ message: "Serverde xeta var" });
+  }
+});
+
+export default router;
